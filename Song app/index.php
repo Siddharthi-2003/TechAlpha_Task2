9944279674ca985a0d715f99467ac6fca4c70043
Siddharthi Saha <106994574+Siddharthi-2003@users.noreply.github.com>
2024-07-14 03:24:57 +0530

    Add files via upload

---
+++ Song app/index.php
@@ -0,0 +1,134 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Spotify</title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.1/css/all.css" integrity="sha384-50oBUHEmvpQ+1lW4y57PTFmhCaXp0ML5d60M1M7uH2+nqUivzIebhndOJK28anvf" crossorigin="anonymous">
+    <link rel="stylesheet" href="Style.css">
+  </head>
+  <body>
+
+    <aside class="sidenav">
+      <div class="logo">
+        <a href="#"><img class="pb-50" src="logorgb.png" alt=""></a>
+      </div>
+      <div class="menulink">
+        <a href="#"><i class="fas fa-home"></i><span>Home</span></a>
+        <a href="#"><i class="fas fa-search"></i> <span>About</span> </a>
+        <a href="#"><i class="fas fa-book pb-50"></i> <span>Your library</span></a>
+      </div>
+      <div class="buttons">
+        <a href="#"><button type="button" class="btn btn-light">Subscribe</button></a>
+        <a href="#"><button type="button" class="btn btn-dark">Sign In</button></a>
+      </div>
+      <div class="cookie col-md-12 text-center">
+        <div class="col-md-4  col-sm-12 ">
+          <a href="#">Cookie Policy</a>
+        </div>
+        <div class="col-md-2 col-sm-12"> | </div>
+        <div class="col-md-6 col-sm-12">
+          <a href="#">Privacy</a>
+        </div>
+      </div>
+    </aside>
+
+    <main>
+      <div class="container-flex ptb-30 plb-20">
+        <div class=" toplist">
+          <ul class="list-inline">
+            <li><a href="#">In Evidence</a></li>
+            <li><a href="#">Podcast</a></li>
+            <li><a href="#">Genere & Moods</a></li>
+            <li><a href="#">New Releases</a></li>
+            <li><a href="#">Discover</a></li>
+          </ul>
+        </div>
+      </div>
+        <div class="title plb-20">
+          <h1><strong>Focus with Your Playlist...</strong></h1>
+        </div>
+      </div>
+        <div class=" container-flex text-center">
+          <div class="music-card">
+            <a href="#"><img src="bhajan.jpg" alt=""></a>
+            <p>BHAJANS</p>
+          </div>
+          <div class="music-card">
+            <a href="#"><img src="go girl.jpg" alt=""></a>
+            <p>GO GIRL</p>
+          </div>
+          <div class="music-card">
+            <a href="#"><img src="kids.jpg" alt=""></a>
+            <p>KIDS MUSIC</p>
+          </div>
+          <div class="music-card">
+            <a href="#"><img src="indie pop.jpg" alt=""></a>
+            <p>INDIE POP</p>
+          </div>
+          <div class="music-card">
+            <a href="#"><img src="kpop.jpg" alt=""></a>
+            <p>KPOP HITS</p>
+          </div>
+          <div class="music-card">
+            <a href="#"><img src="lover.jpg" alt=""></a>
+            <p>FOR LOVE</p>
+          </div>
+          <div class="music-card">
+            <a href="#"><img src="sufi.jpg" alt=""></a>
+            <p>SUFI HITS</p>
+          </div>
+          <div class="music-card">
+            <a href="#"><img src="wedding.jpg" alt=""></a>
+            <p>WEDDING SONGS</p>
+          </div>
+          <div class="music-card">
+            <a href="#"><img src="workout.jpg" alt=""></a>
+            <p>WORKOUT</p>
+          </div>
+          <div class="music-card">
+            <a href="#"><img src="primavera.jpg" alt=""></a>
+            <p>Life</p>
+          </div>
+        </div>
+        <div class="container-flex">
+          <div class="title plb-20">
+            <h1><strong>Classification</strong></h1>
+          </div>
+        </div>
+        <div class="container-flex text-center">
+          <div class="music-card">
+            <a href="#"><img src="top50.jpg" alt=""></a>
+            <p>Top 10 </p>
+          </div>
+          <div class="music-card">
+            <a href="#"><img src="top50.jpg" alt=""></a>
+            <p>Top 50 - Italia</p>
+          </div>
+          <div class="music-card">
+            <a href="#"><img src="top50.jpg" alt=""></a>
+            <p>Top 100 - Italia</p>
+          </div>
+          <div class="music-card">
+            <a href="#"><img src="top50.jpg" alt=""></a>
+            <p>Top choices of you - Italia</p>
+          </div>
+        </div>
+    </main>
+
+
+    <footer>
+      <div class="playmusic text-center">
+        <ul class="list-inline">
+          <li><a href="#"><i class="fas fa-random"></i></a></li>
+          <li><a href="#"><i class="fas fa-step-backward"></i></i></a></li>
+          <li><a href="#"><i class="far fa-play-circle"></i></a></li>
+          <li><a href="#"><i class="fas fa-step-forward"></i></a></li>
+          <li><a href="#"><i class="fas fa-redo"></i></a></li>
+        </ul>
+      </div>
+
+    </footer>
+  </body>
+</html>
